645744d5ee44a888596be369c554d65efee8af2d
iricehasan <hsn.irice@gmail.com>
2026-08-07 23:21:46 +0300

    feat: add stg_tracks.sql

---
+++ models/staging/stg_tracks.sql
@@ -0,0 +1,13 @@
+-- models/staging/stg_tracks.sql
+
+select
+    track_id,
+    name as track_name,
+    album_id,
+    media_type_id,
+    genre_id,
+    composer as composer_name,
+    milliseconds / 1000.0 as duration_seconds,
+    round(bytes / 1048576.0, 2) as size_mb,
+    unit_price
+from {{ source('chinook', 'track') }}
