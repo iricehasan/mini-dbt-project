8a890d4b52534ae9702ead352d683f0398d4e624
iricehasan <hsn.irice@gmail.com>
2026-08-08 00:37:27 +0300

    feat: add agg_artist_revenue.sql in marts

---
+++ models/marts/agg_artist_revenue.sql
@@ -0,0 +1,8 @@
+-- models/marts/agg_artist_revenue.sql
+
+select
+    artist_name,
+    sum(quantity * sale_unit_price) as total_revenue
+from {{ ref('fct_sales') }}
+group by 1
+order by total_revenue desc
