c5a21e025d5c697b76deb79fdb7398eb0a343753
iricehasan <hsn.irice@gmail.com>
2026-08-08 00:40:38 +0300

    models/marts/agg_album_sales.sql

---
+++ models/marts/agg_album_sales.sql
@@ -0,0 +1,9 @@
+-- models/marts/agg_album_sales.sql
+
+select
+    album_title,
+    sum(quantity * sale_unit_price) as total_revenue,
+    sum(quantity) as units_sold
+from {{ ref('fct_sales') }}
+group by 1
+order by total_revenue desc
