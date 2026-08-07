19e25f3ed4b0314af5f0993a68a7735902111735
iricehasan <hsn.irice@gmail.com>
2026-08-08 00:43:58 +0300

    models/marts/agg_monthly_sales.sql

---
+++ models/marts/agg_monthly_sales.sql
@@ -0,0 +1,9 @@
+-- models/marts/agg_monthly_sales.sql
+
+select
+    date_trunc('month', invoice_date) as sales_month,
+    sum(quantity * sale_unit_price) as total_revenue,
+    count(distinct invoice_line_id) as line_items_sold
+from {{ ref('fct_sales') }}
+group by 1
+order by sales_month
